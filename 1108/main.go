231d013740fa1a47279be5e79cb50c19c4cf5a9a
lionkingofnorth <luoyang19922@gmail.com>
2020-04-01 22:46:59 +0800

    [ update ] seconde method

--- 1108/main.go
+++ 1108/main.go
@@ -13,7 +13,19 @@ func main() {
 	// for i, v := range ip {
 	// 	fmt.Printf("i: %d v:%T ", i, v)
 	// }
+	//Method 1
 	decode = strings.ReplaceAll(ip, ".", "[.]")
 	fmt.Println("ip: ", ip)
 	fmt.Println("decode: ", decode)
+	//Method 2
+	var decode2 string
+	for _, v := range ip {
+		if string(v) == "." {
+			decode2 += string("[.]")
+		} else {
+			decode2 += string(v)
+		}
+	}
+	fmt.Println("decode2: ", decode2)
+
 }
